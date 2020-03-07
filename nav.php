e655af62d959055dc092c7da479b20b491f1c23b
Eduardo Donato <zoroah@Outlook.com>
2020-03-07 04:34:59 -0300

    .

---
+++ nav.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<div class="container">
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class=" p-4" style="background-color: #dc3545;">
+      <h4 class="text-white">Cine Sâo Carlos</h4>
+      <span class=" text-white">O melhor em entretenimento e diversão para toda familia.</span>
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link text-white"  href="#">Em Cartaz <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Próximos Filmes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  text-white " href="#">Preços</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link  text-white " href="#">Corporativo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  text-white " href="#">Sessões Extras</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  text-white " href="#">Sala de exibição</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  text-white " href="#">Contato</a>
+      </li>
+    </ul>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark p-2"  style="background-color: #dc3545;">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    
+  </nav>
+</div>
+</div>
+        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
